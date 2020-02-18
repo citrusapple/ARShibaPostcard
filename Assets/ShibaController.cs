da51417d1d1e2e4fbe7d26acd40be0a9598e0419
citrusapple <wng2014@gmail.com>
2020-02-17 23:23:12 -0800

    testing out crossplatforminput with standard unity assets in place of joysticks.  Created new scene for testing

---
+++ Assets/ShibaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ShibaController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, y);
+        rb.velocity = movement * 4f;
+
+        if ( x != 0 && y !=0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y);
+        }
+    }
+}
