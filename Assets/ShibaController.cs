807b77915781e6cc41ffd734cfb2c8af7a659285
citrusapple <wng2014@gmail.com>
2020-04-02 19:39:35 -0700

    changed axis for shiba to be moving only on the plane perpendicular to the postcard.  Still having issues with turns and animation.  changed speed of movement from 4f to 10f

--- Assets/ShibaController.cs
+++ Assets/ShibaController.cs
@@ -6,24 +6,30 @@ using UnityStandardAssets.CrossPlatformInput;
 public class ShibaController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Animation anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = CrossPlatformInputManager.GetAxis("Horizontal");
-        float y = CrossPlatformInputManager.GetAxis("Vertical");
+        float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3(x, y);
-        rb.velocity = movement * 4f;
+        Vector3 movement = new Vector3(x, y, 0);
+        rb.velocity = movement * 10f;
 
-        if ( x != 0 && y !=0)
+        if (x != 0 && y != 0)
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, Mathf.Atan2(x, y) + Mathf.Rad2Deg);
+        }
+        if (x != 0 || y != 0)
+        {
+            anim.Play("backBone|backBoneAction.001");
         }
     }
 }
